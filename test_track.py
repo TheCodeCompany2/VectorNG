73c5067ca573d5d4011ad6d8c1f172f11ebd4564
TheCodeCompany2 <coolbrihat23@gmail.com>
2026-05-24 20:48:24 +0400

    Update build_track()

--- test_track.py
+++ test_track.py
@@ -21,3 +21,5 @@ def build_track():
     Entity(model = "cube", color = color.rgb(0.16,0.16,0.16), scale = (7.4, 0.05,200), position = (0,-0.28,0))
     Entity(model='cube', color = color.rgb(0.13,0.37,0.13), scale = (2,0.04,200), position = (-4.75,-0.27, 0))
     Entity(model='cube', color = color.rgb(0.13,0.37,0.13), scale = (2,0.04,200), position = (4.5,-0.27, 0))
+    Entity(model='cube', color = color.rgb(0.5,0.5,0.5), scale = (0.3,0.8,200), position = (-5.75, 0, 0))
+    Entity(model='cube', color = color.rgb(0.5,0.5,0.5), scale = (0.3,0.8,200), position = (5, 0, 0))
