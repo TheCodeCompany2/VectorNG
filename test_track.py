05abf1d92f79ef95728b37742da7c7163b6b71cc
TheCodeCompany2 <coolbrihat23@gmail.com>
2026-05-24 21:16:09 +0400

    Updated build_track()

--- test_track.py
+++ test_track.py
@@ -18,8 +18,15 @@ from ursina import *
 engine = Ursina()
 
 def build_track():
+#straight1
     Entity(model = "cube", color = color.rgb(0.16,0.16,0.16), scale = (7.4, 0.05,200), position = (0,-0.28,0))
+#runoffs   
     Entity(model='cube', color = color.rgb(0.13,0.37,0.13), scale = (2,0.04,200), position = (-4.75,-0.27, 0))
     Entity(model='cube', color = color.rgb(0.13,0.37,0.13), scale = (2,0.04,200), position = (4.5,-0.27, 0))
+#barriers
     Entity(model='cube', color = color.rgb(0.5,0.5,0.5), scale = (0.3,0.8,200), position = (-5.75, 0, 0))
     Entity(model='cube', color = color.rgb(0.5,0.5,0.5), scale = (0.3,0.8,200), position = (5, 0, 0))
+#hairpin apron(track)
+    Entity(model = "cube", color = color.rgb(0.16,0.16,0.16), scale = (14, 0.05, 20), position = (0, -0.28, 107))
+#straight2
+    Entity(model = "cube", color = color.rgb(0.16,0.16,0.16), scale = (7.4, 0.05,200), position = (9,-0.28,0))
