5f3568adb929f9a0cc13358ccd9198d1709774bb
TheCodeCompany2 <coolbrihat23@gmail.com>
2026-05-29 20:46:11 +0400

    Updated turn1 apron

--- test_track.py
+++ test_track.py
@@ -28,12 +28,26 @@ def build_track():
     Entity(model='cube', color = color.rgb(0.475,0.475,0.475), scale = (0.3,0.8,280), position = (-5.75, 0, 0))
     Entity(model='cube', color = color.rgb(0.475,0.475,0.475), scale = (0.3,0.8,200), position = (5, 0, 0))
 #turn1 apron(track)
-    Entity(model = "cube", color = color.rgb(0.16,0.16,0.16), scale = (36, 0.05, 25), position = (13, -0.28, 107))
+    road_vertices, road_triangles = [],[]
+    
+    for i in range(31):
+        angle = math.radians((i/30)*180)
+        outer_x = 12.5 - (16.2 * math.cos(angle))
+        outer_z = 100.0 + (16.2 * math.sin(angle))
+        inner_x = 12.5 - (8.8 * math.cos(angle))
+        inner_z = 100.0 + (8.8 * math.sin(angle))
+        road_vertices.extend([Vec3(inner_x,-0.28,inner_z),Vec3(outer_x,-0.28,outer_z)])
+    
+        if i < 30:
+            c = i*2
+            road_triangles.extend([(c,c+1,c+2),(c+2,c+1,c+3)])
+    
+    Entity(model = Mesh(vertices=road_vertices, triangles = road_triangles, mode = 'triangle'),color = color.rgb(0.16,0.16,0.16), double_sided = True)
 #outside apex kerb turn1
     num_segments = 30
-    outer_radius = 18.5
-    center_x = 13
-    center_z = 101.0
+    outer_radius = 18.1
+    center_x = 12.5
+    center_z = 100.0
 
     for i in range(num_segments + 1):
         angle_radians = math.radians((i/num_segments)*180)
@@ -50,14 +64,14 @@ def build_track():
         )
 #inside apex kerb turn1
     num_segments = 30
-    outer_radius = 7.5
-    center_x = 13
-    center_z = 101.0
+    inner_radius = 7.5
+    center_x = 12.5
+    center_z = 100.0
 
     for i in range(num_segments + 1):
         angle_radians = math.radians((i/num_segments)*180)
-        x_pos = center_x + (outer_radius * math.cos(angle_radians))
-        z_pos = center_z + (outer_radius * math.sin(angle_radians))
+        x_pos = center_x + (inner_radius * math.cos(angle_radians))
+        z_pos = center_z + (inner_radius * math.sin(angle_radians))
         angle_degrees = 90 - math.degrees(angle_radians)
 
         Entity(
@@ -69,9 +83,9 @@ def build_track():
         )
 #gravel trap for outside turn1
     num_segments = 30
-    gravel_radius = 19.5
+    gravel_radius = 20.1
     center_x = 12.5
-    center_z = 101.0
+    center_z = 100.0
 
     for i in range(num_segments + 1):
         angle_radians = math.radians((i/num_segments)*180)
